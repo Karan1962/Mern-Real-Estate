9ab60434d6c907685264d241a3c40d7dd9d8e1e5
karan <karansawan5050@gmail.com>
2023-10-07 11:21:06 +0530

    auth error handle

--- Backend/Controllers/Auth.controller.js
+++ Backend/Controllers/Auth.controller.js
@@ -1,11 +1,16 @@
 import User from '../Models/User.model.js'
+import bcryptjs from 'bcryptjs'
 
 export const signUpAuth = async (req,res)=>{
-  const {userName,password,email} = req.body;
-  
+  try{ const {userName,password,email} = req.body;
+  const hashPassword = bcryptjs.hashSync(password);
   const newUser = new User({
-    userName,email,password
+    userName,email,password:hashPassword
   });
   await newUser.save();
-  res.status(201).json();
+  res.status(201).json('user created successfully');}
+  catch(error){
+    res.status(500).json('something went wrong!')
+  }
+ 
 }
