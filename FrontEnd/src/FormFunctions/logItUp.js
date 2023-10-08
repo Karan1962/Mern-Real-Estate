d03f8c452f0659e8e6286326ed4311149bc5a3ce
karan <karansawan5050@gmail.com>
2023-10-08 23:37:48 +0530

    add axios to login

--- FrontEnd/src/FormFunctions/logItUp.js
+++ FrontEnd/src/FormFunctions/logItUp.js
@@ -1,14 +1,12 @@
-export const logItUp = async (userName , passWord)=>{
-    try {
-        const response = await fetch("/api/user", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ userName, password: passWord }),
-        });
-        return response.status;
-      } catch (error) {
-        console.error("Error:", error);
-      }
-}
+import axios from "axios";
+export const logItUp = async (userName, passWord) => {
+  try {
+    const response = await axios.post("/api/user", {
+      userName,
+      password: passWord,
+    });
+    return response.status;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
