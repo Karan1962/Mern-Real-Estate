38d447170b27d1aaf428379bd91b2b556ec183b1
karan <karansawan5050@gmail.com>
2023-10-08 22:47:20 +0530

    add axios

--- FrontEnd/src/FormFunctions/signUp.js
+++ FrontEnd/src/FormFunctions/signUp.js
@@ -1,11 +1,10 @@
+import axios from "axios";
 export const signItUp = async (userName, passWord, email) => {
   try {
-    const response = await fetch("/api/auth/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userName, password: passWord, email }),
+    const response = await axios.post("/api/auth/signup", {
+      userName,
+      password: passWord,
+      email,
     });
     return response.status;
   } catch (error) {
