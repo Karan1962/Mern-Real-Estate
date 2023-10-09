4c1634d12c340226fc6d46c2a29975147d4cf2ac
karan <karansawan5050@gmail.com>
2023-10-09 12:56:17 +0530

    About page add

--- FrontEnd/src/Pages/About.jsx
+++ FrontEnd/src/Pages/About.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="w-full">
+      <div className="max-w-6xl m-auto px-3 pt-24">
+        <h1 className="text-3xl font-bold text-slate-700 ">
+          About Only Estate
+        </h1>
+        <p className="py-4 text-gray-600">
+          Only Estate is a leading real estate agency that specializes in
+          helping clients buy, sell, and rent properties in the most desirable
+          neighborhoods. Our team of experienced agents is dedicated to
+          providing exceptional service and making the buying and selling
+          process as smooth as possible.
+        </p>
+        <p className="pb-4 text-gray-600">
+          Our mission is to help our clients achieve their real estate goals by
+          providing expert advice, personalized service, and a deep
+          understanding of the local market. Whether you are looking to buy,
+          sell, or rent a property, we are here to help you every step of the
+          way.
+        </p>
+        <p className="text-gray-600">
+          Our team of agents has a wealth of experience and knowledge in the
+          real estate industry, and we are committed to providing the highest
+          level of service to our clients. We believe that buying or selling a
+          property should be an exciting and rewarding experience, and we are
+          dedicated to making that a reality for each and every one of our
+          clients.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
